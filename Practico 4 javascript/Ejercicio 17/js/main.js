64c7780981949e33c42274b7b5db186c9525097c
Exequiel Hernando <exequielhernando@gmail.com>
2019-04-10 16:55:11 -0300

    Practico 17 listo

--- Practico 4 javascript/Ejercicio 17/js/main.js
+++ Practico 4 javascript/Ejercicio 17/js/main.js
@@ -1,17 +1,39 @@
 "use strict";
-let btn1 =  document.getElementById("btn1");
-let btn2 =  document.getElementById("btn2");
-let btn3 =  document.getElementById("btn3");
-btn1.addEventListener("click", function(e){
-  mostrarBoton(1);
-});
-btn2.addEventListener("click", function(e){
-  mostrarBoton(2);
-});
-btn3.addEventListener("click", function(e){
-  mostrarBoton(3);
-});
-function mostrarBoton(valor) {
-  let nodoBotonAMostrar = document.getElementById("oculto" + valor);
-  nodoBotonAMostrar.style.display = "block";
+
+let categorias = document.getElementById("categorias");
+let deportes = document.getElementById("deportes");
+let contacto =  document.getElementById("contacto");
+
+let botones = document.getElementsByClassName("boton-contenido");
+let contenido = document.getElementsByClassName("contenido");
+
+/* Cargamos la funcion para que se oculte el contenido ni bien
+se inicia la pagina*/
+ocultarContenido();
+/* Cargamos la funcion para que detecte el click en algun boton*/
+setBotonesClick();
+/* Setea los botones para que al hacer click si NO tiene clase
+contenido-oculto le agrege esa misma clase y se ocultarContenido
+y si TIENE contenido-oculto entonces le agrega la clase a todas y
+despues remueve la clase contenido oculto de si mismo */
+function setBotonesClick() {
+  for (var i = 0; i < botones.length; i++) {
+
+    botones[i].onclick = function(e){
+      if (!this.classList.contains('contenido-oculto')) {
+        this.classList.add('contenido-oculto');
+      }
+      else {
+        ocultarContenido();
+        this.classList.remove('contenido-oculto');
+      }
+    }
+  }
+}
+function ocultarContenido(){
+  for (var i = 0; i < botones.length; i++) {
+    if (!botones[i].classList.contains('contenido-oculto')) {
+      botones[i].classList.add('contenido-oculto');
+    }
+  }
 }
